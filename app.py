9adc58239357f3e3b2526c84edcd5ca8e3585a1a
Atanas Delevski <adelevski@gmail.com>
2021-02-19 02:08:19 -0600

    update 2

--- app.py
+++ app.py
@@ -1,14 +1,40 @@
-from flask import Flask
+from flask import Flask, render_template, request, flash, redirect, jsonify
+from binance.client import Client
+from binance.enums import *
+import config, csv, datetime
 
 app = Flask(__name__)
+app.secret_key = config.flask_secret_key
+
+client = Client(config.API_KEY, config.API_SECRET, tld='us')
 
 @app.route('/')
 def index():
-    return "Index"
+    title = "QuickTick"
 
-@app.route('/buy')
+    account = client.get_account()
+
+    balances = account['balances']
+    
+    exchange_info = client.get_exchange_info()
+
+    symbols = exchange_info['symbols']
+
+    return render_template('index.html', title=title, my_balances=balances, symbols=symbols)
+
+@app.route('/buy', methods=['POST'])
 def buy():
-    return 'buy'
+    print(request.form)
+    try:
+        order = client.create_order(symbol=request.form['symbol'],
+            side=SIDE_BUY,
+            type=ORDER_TYPE_MARKET,
+            quantity=request.form['quantity'])
+    except Exception as e:
+        flash(e.message, "error")
+
+
+    return redirect('/')
 
 @app.route('/sell')
 def sell():
@@ -17,3 +43,23 @@ def sell():
 @app.route('/settings')
 def settings():
     return 'settings'
+
+
+@app.route('/history')
+def history():
+    candlesticks = client.get_historical_klines("BTCUSDT", Client.KLINE_INTERVAL_15MINUTE, "7 Feb, 2021", "19 Feb, 2021")
+
+    processed_candlesticks = []
+
+    for data in candlesticks:
+        candlestick = {
+            "time": data[0] / 1000,
+            "open": data[1],
+            "high": data[2],
+            "low": data[3],
+            "close": data[4]
+        }
+        
+        processed_candlesticks.append(candlestick)
+
+    return jsonify(processed_candlesticks)
--- config.py
+++ config.py
@@ -11,3 +11,6 @@ ameritrade = 'OBUOMRLDK7GQHO05DREJ15M8V6ICB1XB'
 API_KEY = 'XxCDYBtlO9qiCXhyyHImXaIw7fML71GLCBjSNMfVsafJXrEfSksRGIB97Le4cb40'
 API_SECRET = 'LC1T3AulG97Oz7EuM3fmpjRYM2372heJDeKSAesFxjwg7iYDkKKNTYbEKiFSO7PW'
 
+# Flask
+flask_secret_key = b'qwertyuiopasdfghjklzxcvbnm'
+
