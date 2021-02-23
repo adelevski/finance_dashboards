e5dad0367cd3cf6f9f326fd05cda1124875ca4f2
Atanas Delevski <adelevski@gmail.com>
2021-02-22 23:56:48 -0600

    updatinggg

--- TA_Screener/ta_app.py
+++ TA_Screener/ta_app.py
@@ -1,14 +1,22 @@
 from flask import Flask, render_template
 from patterns import patterns
+import yfinance as yf
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template('ta_index.html', patterns=patterns)
 
 @app.route('/snapshot')
 def snapshot():
+    # with open('TA_Screener\datasets\companies.csv') as f:
+    #     companies = f.read().splitlines()
+    #     for company in companies:
+    #         symbol = company.split(',')[0]
+    #         df = yf.download(symbol, start="2020-06-22", end="2021-02-22")
+    #         df.to_csv(f'TA_Screener\datasets\daily\{symbol}.csv')
+
     return {
-        'code': "sucess"
+        'code': "success"
     }
